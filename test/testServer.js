890d70301423bc2d0b618e7bc4fc726a0122a76a
t ddddddd <miscmisc@cmme.org>
2014-01-29 17:06:09 -0800

    remove test server output

--- test/testServer.js
+++ test/testServer.js
@@ -12,12 +12,10 @@ app.use(function(req, res) {
 
 module.exports.startServer = function(port, path, callback) {
   server = http.createServer(app).listen(port, function() {
-    console.log('Server listening on port ' + port);
     callback();
   });
 };
 
 module.exports.stopServer = function(callback) {
-  console.log("Stopping server");
   server.close(callback);
 };
